c237d17825fe11af0b5569c822ea56110f1d1ecc
Poonam-Gorai <poonamgorai1807@gmail.com>
2021-09-27 20:03:07 +0530

    second commit

--- script.js
+++ script.js
@@ -3,14 +3,14 @@
 function filterNames() {
 
   //get input element
-   let inputGroupName = document.getElementById("inputGroupName");
+  let inputGroupName = document.getElementById("inputGroupName");
   //get values of input
-   let filterValue = inputGroupName.value.toUpperCase();
+  let filterValue = inputGroupName.value.toUpperCase();
   //get names
-   let tablename = document.getElementById("names");
-   let tablevalue = tablename.getElementsByTagName("tr");
+  let tablename = document.getElementById("names");
+  let tablevalue = tablename.getElementsByTagName("tr");
   //loop through tablevalues
-  for ( let i = 0; i < tablevalue.length; i++) {
+  for (let i = 0; i < tablevalue.length; i++) {
     let td = tablevalue[i].getElementsByTagName("td")[0];
     if (td) {
       let txt = td.textContent;
@@ -22,6 +22,18 @@ function filterNames() {
     }
   }
 }
+
+//debounce
+
+function debounce(func, timeout = 1000){
+  let timer;
+  return (...args) => {
+  clearTimeout(timer);
+  timer = setTimeout(() => { func.apply(this, args); }, timeout);
+  };
+  }
+  const waitForInput = debounce(() => filterNames());
+
 
 //moving left and right
 
@@ -35,6 +47,9 @@ ButtonLeft.onclick = function () {
   document.getElementById("selectGroupsId").scrollLeft -= 50;
 };
 
+
+
+
 //sidebar
 function showMenu() {
   let element = document.getElementById("sidebar");
@@ -42,49 +57,56 @@ function showMenu() {
 }
 
 
+
+
 //sorting
 
 function sortTableByGroupName(table, column, asc = true) {
-  // checking if asc or not
+  // direction modifier - checking if asc or not
   const dirModifier = asc ? 1 : -1;
-  // single tbody in case we have many tbody in a table
+  // tbody in case we have many tbody in a html
   const tBody = table.tBodies[0];
-  // converts the data into arry form list
+  // The data into array form list
   const rows = Array.from(tBody.querySelectorAll("tr"));
 
   // sort each row
   const sortedRows = rows.sort((a, b) => {
+    //console.log(a);
+    //console.log(b);
+    //get the text content from the column
     const aColText = a.querySelector("td").textContent.trim();
     const bColText = b.querySelector("td").textContent.trim();
 
-    // if td1 > td2 then multiply by 1 to dirModifier to remain as it is and if not then multiply by -1 for desending
-    return aColText > bColText ? 1 * dirModifier : -1 * dirModifier;
+    //console.log(aColText);
+    //console.log(bColText);
+
+    // if td1 > td2 then 1 times direction Modifier to remain same and if not then -1 times the direction modifier for desending
+    return aColText > bColText ? (1 * dirModifier) : (-1 * dirModifier);
   });
 
-  //   remove all the existing trs from the table
+  //   Remove all the existing trs from the table
   while (tBody.firstChild) {
     tBody.removeChild(tBody.firstChild);
   }
 
-  //   Now adding trs back in the table after sorting
+  //Re-adding the newly sorted rows
   tBody.append(...sortedRows);
 
-  //   remember the sorting order
-  table
-    .querySelectorAll("th")
-    .forEach((th) => th.classList.remove("th-sort-asc", "th-sort-desc"));
-  //   toggle between the sort classes
+  //   Remember how the column is currently sorted.
+  table.querySelectorAll("th").forEach((th) => th.classList.remove("th-sort-asc", "th-sort-desc"));
+  //   class toggle between
   table.querySelector("th").classList.toggle("th-sort-asc", asc);
   table.querySelector("th").classList.toggle("th-sort-desc", !asc);
 }
 
-let whichSort = document.getElementById("sorting");
-// calling the function on click to trigger the sorting
-whichSort.addEventListener("click", function () {
-  // gets the element to check which class is implemented on it
+let Sort = document.getElementById("sorting");
+// adding event listener
+Sort.addEventListener("click", function () {
+ 
+  let tableElement = document.querySelector("table");
   let checkOrder = document.getElementsByTagName("th")[0];
   let currentIsAscending = checkOrder.classList.contains("th-sort-asc");
-  let tableElement = document.querySelector("table");
+  
   sortTableByGroupName(tableElement, 1, !currentIsAscending);
 });
 
